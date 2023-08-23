a5d7f0417ff1676896f59ca4cef8c44648840ebf
Srimathi Padmanaban <126326522+Srimathi-1106@users.noreply.github.com>
2023-08-23 21:30:33 +0530

    Add files via upload

---
+++ Day28/Solution.java
@@ -0,0 +1,25 @@
+//LEETCODE PROBLEM- 167. Two Sum II - Input Array Is Sorted
+
+class Solution {
+    public int[] twoSum(int[] n, int t) {
+        int l=0,r=n.length-1;
+        while(l<r)
+        {
+            if(n[l]+n[r]>t)
+            {
+                r--;
+            }
+            else if(n[l]+n[r]==t)
+            {
+                break;
+            }
+            else
+            {
+                l++;
+            }
+        }
+        return new int[] {l+1,r+1};
+    }
+}
+
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
