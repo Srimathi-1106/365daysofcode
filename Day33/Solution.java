574ca0f77db8fd4f937ad4f991231a1d6af1d6c7
Srimathi Padmanaban <126326522+Srimathi-1106@users.noreply.github.com>
2023-08-28 21:21:20 +0530

    Add files via upload

---
+++ Day33/Solution.java
@@ -0,0 +1,19 @@
+//LEETCODE PROBLEM - 2810. Faulty Keyboard
+
+class Solution {
+    public String finalString(String s) {
+        StringBuilder str=new StringBuilder();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='i')
+            {
+                str.reverse();
+            }
+            else
+            {
+                str.append(s.charAt(i));
+            }
+        }
+        return str.toString();
+    }
+}
