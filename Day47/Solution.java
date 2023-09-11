1283501a66ffd8670c10784fc4cfdf98506386f8
Srimathi Padmanaban <126326522+Srimathi-1106@users.noreply.github.com>
2023-09-11 22:58:11 +0530

    Add files via upload

---
+++ Day47/Solution.java
@@ -0,0 +1,13 @@
+// LEETCODE PROBLEM - 2778. Sum of Squares of Special Elements
+
+class Solution {
+    public int sumOfSquares(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums.length%(i+1)==0)
+            sum+=nums[i]*nums[i];
+        }
+        return sum;
+    }
+}
