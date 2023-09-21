80432b0e57082aa613383bf915cd706a9f617597
Srimathi Padmanaban <126326522+Srimathi-1106@users.noreply.github.com>
2023-09-21 21:32:48 +0530

    Add files via upload

---
+++ Day57/Solution.java
@@ -0,0 +1,21 @@
+// LEETCODE PROBLEM - Reverse Words in a String
+
+class Solution {
+    public String reverseWords(String s) {
+        String str[]=s.split(" ");
+        for(int i=0;i<str.length/2;i++)
+        {
+            String a=str[i];
+            str[i]=str[str.length-1-i];
+            str[str.length-1-i]=a;
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<str.length-1;i++)
+        {
+            if(!str[i].equals(""))
+            sb.append(str[i].strip()+" ");
+        }
+        sb.append(str[str.length-1]);
+        return sb.toString().strip();
+    }
+}
