e8d1fe7282add6676e046a901fe442d59fc370e8
Srimathi Padmanaban <126326522+Srimathi-1106@users.noreply.github.com>
2023-10-09 23:41:30 +0530

    Add files via upload

---
+++ Day75/Solution.java
@@ -0,0 +1,15 @@
+//LEETCODE PROBLEM - 2652. Sum Multiples
+
+class Solution {
+    public int sumOfMultiples(int n) {
+        int sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%3==0 || i%5==0 || i%7==0)
+            {
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+}
