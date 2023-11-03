917f265fb4c650781629dc44abcebd36e5c6c755
Srimathi Padmanaban <126326522+Srimathi-1106@users.noreply.github.com>
2023-11-03 20:14:01 +0530

    Add files via upload

---
+++ Day100/Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> res = new ArrayList<String>();
+        int a = 1;
+        for (int i = 0; i < target.length; i++) {
+            if (target[i] == a) {
+                res.add("Push");
+            } else {
+                while (a < target[i]) {
+                    res.add("Push");
+                    res.add("Pop");
+                    a++;
+                }
+                res.add("Push");
+            }
+            a++;
+        }
+        return res;
+    }
+}
