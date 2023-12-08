47465c588d114048cf6402a558851762ef078a15
Srimathi Padmanaban <126326522+Srimathi-1106@users.noreply.github.com>
2023-12-08 21:08:20 +0530

    Add files via upload

---
+++ Day135/Solution.java
@@ -0,0 +1,13 @@
+// LEETCODE PROBLEM - 2951. Find the Peaks
+
+class Solution {
+    public List<Integer> findPeaks(int[] mountain) {
+        List<Integer> peaks=new ArrayList<>();
+        for(int i=1;i<mountain.length-1;i++)
+        {
+            if(mountain[i]>mountain[i-1] && mountain[i]>mountain[i+1])
+            peaks.add(i);
+        }
+        return peaks;
+    }
+}
