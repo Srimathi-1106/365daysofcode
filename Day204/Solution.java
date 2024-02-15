d05c29716adbc67493cd1d0abf24e0a2c04c0097
Srimathi Padmanaban <126326522+Srimathi-1106@users.noreply.github.com>
2024-02-15 21:45:56 +0530

    Add files via upload

---
+++ Day204/Solution.java
@@ -0,0 +1,11 @@
+// LEETCODE PROBLEM - 292. Nim Game
+
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0)
+        {
+            return false;
+        }
+        return true;
+    }
+}
