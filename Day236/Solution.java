01338bdda978352c1455f81de58cfe8972baeb25
Srimathi Padmanaban <126326522+Srimathi-1106@users.noreply.github.com>
2024-03-18 21:51:20 +0530

    Update Solution.java

--- Day236/Solution.java
+++ Day236/Solution.java
@@ -2,7 +2,7 @@
 
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points,(a,b)->Integer.compare(a[],b[1]));
+        Arrays.sort(points,(a,b)->Integer.compare(a[1],b[1]));
         int count=1,end=points[0][1];
         for(int i=1;i<points.length;i++)
         {
