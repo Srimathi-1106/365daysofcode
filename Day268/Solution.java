e6881dcd32abb5a88fd383c9fe0e731e1b852396
Srimathi Padmanaban <126326522+Srimathi-1106@users.noreply.github.com>
2024-04-19 18:15:46 +0530

    Add files via upload

---
+++ Day268/Solution.java
@@ -0,0 +1,34 @@
+// LEETCODE PROBLEM - 200. Number of Islands
+
+class Solution {
+    public void dfs(char grid[][],int i,int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length)
+        {
+            return;
+        }
+        if(grid[i][j]=='0')
+        {
+            return;
+        }
+        grid[i][j]='0';
+        dfs(grid,i,j-1);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i+1,j);
+    }
+    public int numIslands(char[][] grid) {
+        int count=0;
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    count++;
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        return count;
+    }
+}
