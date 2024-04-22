68c58e6df888a3d58da06b5dff3f31225c8e491e
Srimathi Padmanaban <126326522+Srimathi-1106@users.noreply.github.com>
2024-04-22 22:40:15 +0530

    Add files via upload

---
+++ Day271/Solution.java
@@ -0,0 +1,39 @@
+// LEETCODE PROBLEM - 3120. Count the Number of Special Characters I
+
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int fre[]=new int[26];
+        int count=0;
+        for(char character:word.toCharArray())
+        {
+            if(Character.isUpperCase(character))
+            {
+                if(fre[character-65]==1)
+                {
+                    fre[character-65]=3;
+                }
+                else if(fre[character-65]==0)
+                {
+                    fre[character-65]=2;
+                }
+            }
+            else
+            {
+                if(fre[character-97]==2)
+                {
+                    fre[character-97]=3;
+                }
+                else if(fre[character-97]==0)
+                {
+                    fre[character-97]=1;
+                }
+            }
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(fre[i]==3)
+            count++;
+        }
+        return count;
+    }
+}
