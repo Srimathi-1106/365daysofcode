d824c016c427c087f058cba5136ed5135f97bfe4
Srimathi Padmanaban <126326522+Srimathi-1106@users.noreply.github.com>
2024-05-19 22:30:44 +0530

    Add files via upload

---
+++ Day298/Solution.java
@@ -0,0 +1,16 @@
+// LEETCODE PROBLEM - 2824. Count Pairs Whose Sum is Less than Target
+
+class Solution {
+    public int countPairs(List<Integer> nums, int target) {
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums.get(i)+nums.get(j)<target)
+                count++;
+            }
+        }
+        return count;
+    }
+}
