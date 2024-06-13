f3f12e25aa69b558dd8b76300f1b10924d69dcaf
Srimathi Padmanaban <126326522+Srimathi-1106@users.noreply.github.com>
2024-06-13 20:01:27 +0530

    Add files via upload

---
+++ Day323/Solution.java
@@ -0,0 +1,14 @@
+// LEETCODE PROBLEM - 2037. Minimum Number of Moves to Seat Everyone
+
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int count=0;
+        for(int i=0;i<seats.length;i++)
+        {
+            count+=Math.abs(seats[i]-students[i]);
+        }
+        return count;
+    }
+}
