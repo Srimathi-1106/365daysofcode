27623097ef680d44c6b30fc337003e58a8230d4c
Srimathi Padmanaban <126326522+Srimathi-1106@users.noreply.github.com>
2024-07-10 20:38:34 +0530

    Add files via upload

---
+++ Day350/Solution.java
@@ -0,0 +1,20 @@
+// LEETCODE PROBLEM - 1598. Crawler Log Folder
+
+class Solution {
+    public int minOperations(String[] logs) {
+        int count=0;
+        for(String log:logs)
+        {
+            if(log.equals("../"))
+            {
+                if(count>0)
+                count--;
+            }
+            else if(!log.equals("./"))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
