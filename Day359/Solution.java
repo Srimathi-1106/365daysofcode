3fe6411a09f1e9888ee7067111907a773663c1da
Srimathi Padmanaban <126326522+Srimathi-1106@users.noreply.github.com>
2024-07-19 13:40:46 +0530

    Add files via upload

---
+++ Day359/Solution.java
@@ -0,0 +1,34 @@
+// LEETCODE PROBLEM - 1380. Lucky Numbers in a Matrix
+
+class Solution {
+    public List<Integer> luckyNumbers (int[][] matrix) {
+        List<Integer> luckyNumbers=new ArrayList<>();
+        int rlen=matrix.length,clen=matrix[0].length;
+        for(int row=0;row<rlen;row++)
+        {
+            int min=Integer.MAX_VALUE,c=0;
+            boolean isLucky=true;
+            for(int col=0;col<clen;col++)
+            {
+                if(min>matrix[row][col])
+                {
+                    min=matrix[row][col];
+                    c=col;
+                }
+            }
+            for(int tempRow=0;tempRow<rlen;tempRow++)
+            {
+                if(min<matrix[tempRow][c])
+                { 
+                    isLucky=false;
+                    break;
+                }
+            }
+            if(isLucky)
+            luckyNumbers.add(min);
+        }
+        
+        return luckyNumbers;
+
+    }
+}
